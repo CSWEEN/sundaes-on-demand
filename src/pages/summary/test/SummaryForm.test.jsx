9c26b669c2751e0ad47bde79db4088bd75b96ae7
ChrisSweeneyIRIS <chrissweeney.cs@gmail.com>
2022-04-01 15:51:56 +0100

    Popover test created

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
+import { queryByText } from "@testing-library/dom";
 
 describe("Form Tests", () => {
   it("should render checkbox and button in inital state", () => {
@@ -30,5 +31,28 @@ describe("Form Tests", () => {
 
     userEvent.click(checkbox);
     expect(confirmButton).toBeDisabled();
+  });
+
+  it("should have a popover appear upon hover", () => {
+    render(<SummaryForm />);
+
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+
+    expect(nullPopover).toBeNull();
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    userEvent.unhover(termsAndConditions);
+    const nullPopoverAgain = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopoverAgain).not.toBeInTheDocument();
   });
 });
