dc2a45c9573d792c777c7967a05aa5232a79878d
ChrisSweeneyIRIS <chrissweeney.cs@gmail.com>
2022-04-04 14:34:03 +0100

    Topping test created

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 
-export default function ScoopOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath }) {
   return (
-    <Col>
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
         style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
-        alt={`${name} scoop`}
-      ></img>
+        alt={`${name} topping`}
+      />
     </Col>
   );
 }
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,21 @@ it("should display an image for each scoop option from the server", async () => 
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+it("should display an image for each topping option from the server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const imageTitles = toppingImages.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
