228bb088284248128ba6702411fe68ca514242f1
ChrisSweeneyIRIS <chrissweeney.cs@gmail.com>
2022-04-04 16:02:33 +0100

    Renamed test file and fixed failing test
    Was called text instead of test

--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,9 +1,10 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import { Server } from "http";
 import OrderEntry from "../OrderEntry";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
 
-it("should handle errors for scoops and toppings routes", async () => {
-  Server.resetHandlers(
+test("handles error for scoops and toppings routes", async () => {
+  server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
     ),
@@ -11,13 +12,11 @@ it("should handle errors for scoops and toppings routes", async () => {
       res(ctx.status(500))
     )
   );
-  render(<OrderEntry />);
+
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
 
   await waitFor(async () => {
     const alerts = await screen.findAllByRole("alert");
-
     expect(alerts).toHaveLength(2);
   });
 });
-
-it("should fail", () => {});
